f561c3fe8bd92486eb1e865143540740024a99cd
김원형 <whkim@amgine.co.kr>
2024-05-17 00:38:56 +0900

    1. 회원가입 2. 로그인 및 토큰 발행  - accessToken  - refreshToken 3. 토큰 리프레쉬 4. 토큰 검증 미들웨어 추가

--- nodejs/todo-app/index.js
+++ nodejs/todo-app/index.js
@@ -2,20 +2,161 @@ const express = require('express')
 const bodyParser = require("body-parser");
 const app = express()
 
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+//jwt
+const jwt = require('./utils/jwt-util');
+
+// bcrypt
+const bcrypt = require('bcrypt');
+
 // db
 const maria = require('./database/connect/maria');
 maria.connect();
 const port = 3000;
 
+// redis
+const redis = require('redis');
+const redisClient = redis.createClient(process.env.REDIS_PORT);
+
+
+const authJwt = require('./authJWT');
+const router = express.Router();
+
+const refresh = require('./refresh');
+
 // boby parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//signup////////////////////////////////////////////////////////
+// 회원 가입
+app.post('/signup', (req, res) => {
+    console.log("/signup");
+    let params1 = [req.body.user_id];
+    let sql = 'SELECT id FROM todo.user WHERE user_id = ?';
+    maria.query(sql, params1, function(err, rows, fields) {
+        if(!err){
+            console.log(rows.length);
+            if(rows.length == 0){
+                bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(req.body.password, salt, function(err, hash) {
+                        // Store hash in your password DB.
+                        myPass = hash;
+                        let insert_sql = "INSERT INTO todo.user (user_id, password) VALUES (?, ?)";
+                        var params = [req.body.user_id, myPass];
+                        console.log(params);
+                        maria.query(insert_sql, params, function(err, rows, fields) {
+                            if(!err){
+                                console.log("success");
+                                return res.status(200).json({
+                                    msg : '회원가입에 성공하였습니다.'
+                                    //,token : newUserToken 
+                                });
+                            } else{
+                                console.log("err : ", err);
+                                return res.status(400).json({msg : 'db failed'});
+                            }
+                            });
+                        });
+                });
+            }else{
+                return res.status(400).json({msg : '이미 같은 아이디가 존재합니다'});
+            }
+        }
+        else {
+            console.log(false);
+            return res.status(400).json({msg : 'db failed'});
+        }
+      });
+
+    
+})
+//signin////////////////////////////////////////////////////////
+app.post('/signin', (req, res) => {
+    console.log("/signin");
+    const {user_id, password} = req.body;
+    sel_sql = "select * from user where user_id = ?";
+    params = [user_id];
+    maria.query(sel_sql, params, function(err, rows, fileds) {
+        if(!err){
+            if(rows != undefined) {
+                if(rows[0] == undefined) {
+                    res.send(null)
+                } else {
+                    // Load hash from your password DB.
+                    bcrypt.compare(password, rows[0].password, async function(err, result) {
+                        // result == true
+                        if(result == true) {
+                            user = {
+                                id: rows[0].user_id
+                            }
+                            const accessToken = jwt.sign(user);
+                            const refreshToken = jwt.refresh();
+                            
+                            // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
+                            try{
+                                await redisClient.connect();
+                                await redisClient.set(user.id, refreshToken);
+                            } catch (e) {
+                                console.error(e);
+                            }
+                            
+                        
+                            res.status(200).send({ // client에게 토큰 모두를 반환합니다.
+                            ok: true,
+                            data: {
+                                accessToken,
+                                refreshToken,
+                            },
+                            });
+                        } else {
+                            res.send('실패')
+                        }
+                    });
+                }
+            } else {
+                res.send('실패')
+            }
+            
+        }else{
+            res.send('실패')
+        }
+    })
+});
+
+//refresh///////////////////////////////////////////////////////////////
+// JWT 만료 시 새로운 JWT 발행
+app.post('/refresh', refresh)
+
 app.get('/', (req, res) => {
     res.send('Hello')
 })
+
+//check token///////////////////////////////////////////////////////////////
+function authenticateToken(req, res, next) {
+    console.log("authenticateToken");
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (token == null) return res.sendStatus(401);
+    
+    console.log(token);
+    //token 검증
+    verifyResult = jwt.verify(token);
+    console.log(verifyResult);
+    if(verifyResult.ok){
+        req.user = user
+        next();
+    }else{
+        res.sendStatus(401);
+    }
+  }
+
 // To-Do 항목을 읽기
-app.get('/api/v1/todo/readTodo', (req, res) => {
+app.get('/api/v1/todo/readTodo', authenticateToken, (req, res) => {
     console.log("readTodo");
     maria.query('SELECT id, title, contains, complete FROM todo.todo_list WHERE deleted_at is null', function(err, rows, fields) {
         let result = {"status": 200};
@@ -36,7 +177,7 @@ app.get('/api/v1/todo/readTodo', (req, res) => {
 })
 
 // To-Do 항목을 생성
-app.put('/api/v1/todo/createTodo', (req, res) => {
+app.put('/api/v1/todo/createTodo', authenticateToken, (req, res) => {
     // 입력 값 체크
     console.log("createTodo");
     let result = {"status": 200};
@@ -87,7 +228,7 @@ app.put('/api/v1/todo/createTodo', (req, res) => {
 })
 
 // To-Do 항목을 업데이트
-app.post('/api/v1/todo/updateTodo', (req, res) => {
+app.post('/api/v1/todo/updateTodo', authenticateToken, (req, res) => {
     console.log("updateTodo");
     let result = {"status": 200};
     let update_sql = "UPDATE todo.todo_list SET title = ?, contains = ?, complete = ? WHERE id = ?";
@@ -142,7 +283,7 @@ app.post('/api/v1/todo/updateTodo', (req, res) => {
 })
 
 // To-Do 항목을 삭제
-app.delete('/api/v1/todo/deleteTodo', (req, res) => {
+app.delete('/api/v1/todo/deleteTodo', authenticateToken, (req, res) => {
     console.log("deleteTodo");
     
     let result = {"status": 200};
